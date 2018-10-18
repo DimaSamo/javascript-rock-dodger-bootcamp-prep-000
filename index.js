b6a29283db988eef99e9f34c199444cb0af562c0
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-18 19:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     rock.style.top=`${top+=2}px`
+     rock.style.top=`${top+=6}px`
      if (checkCollision(rock)){
        endGame()
      }
