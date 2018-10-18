47cc71c648ae0954bd13748fb654513cb51086c6
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-18 19:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,6 +53,53 @@ function checkCollision(rock) {
   }
 }
 
+function createRock(x) {
+  const rock = document.createElement('div')
+  rock.className = 'rock'
+  rock.style.left = `${x}px`
+
+  // Hmmm, why would we have used `var` here?
+  var top = 0
+
+  rock.style.top = top
+
+  /**
+   * Now that we have a rock, we'll need to append
+   * it to GAME and move it downwards.
+   */
+      GAME.appendChild(rock)
+
+
+
+      function step(){
+        top+=2
+        rock.style.top=`${top}px`
+       if (top < 380){
+         window.requestAnimationFrame(step)
+       }
+      }
+  /**
+   * This function moves the rock. (2 pixels at a time
+   * seems like a good pace.)
+   */
+  function moveRock() {
+    // implement me!
+    // (use the comments below to guide you!)
+    /**
+     * If a rock collides with the DODGER,
+     * we should call endGame()
+     */
+     rock.style.top=`${top+=6}px`
+     if (checkCollision(rock)){
+       endGame()
+     }
+     else if (top<GAME_HEIGHT){
+       window.requestAnimationFrame(moveRock)
+     }
+     else {
+       rock.remove()
+       rocksDodged++
+     }
 
 
 
